36c18690b3c4831c1eb6776eb0ca6d55a6b9867f
Mohammad harb <136888573+MohammadHb82@users.noreply.github.com>
2023-07-08 01:58:50 -0700

    learn about index and slice for string

---
+++ main.py
@@ -0,0 +1,19 @@
+var1="value"
+# print(var1[-2])
+# print(var1[1: ])
+# print(len(var1))
+# print(type(var1))
+# var2=10
+# print(type(var2))
+# var3=True
+# print(type(var3))
+# var4=2.78
+# print(type(var4))
+# comp=complex(8+4j)
+# print(type(comp))
+# print(var1,var2,var3,var4,comp)
+str1="hello"
+str1="y"+str1[1:]
+print(str1)
+str2=str1[0:2]+"mm"+str1[4:]
+print(str2)
