970a5154bea730a5fd0a6806d9865e74459f5bb0
Mohammad harb <morhafalharahshah@gmail.com>
2023-08-02 00:56:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -44,8 +44,119 @@ studant={'name':'','age':0, 'country':'', 'city':'', 'job':'', 'skills':[],'pran
 
 print(student) 
 for i in student
+if type(student[i]) == int
+       studnet[i] = input ('enter the ' + i + ':'))
 
-       studnet[i] = input ('enter the ' + i + ':')
+elif type(student[i]) == list:
+times = int(input('how many skills do you hav'))
 
 
     
+# esraa=input('Please enter a word')
+
+# for n in range  (0,len(esraa)):
+#   if(esraa[n]=='l'or esraa[n]=='u'or esraa[n]=='L' or esraa[n]=='U'):
+#      print('*')
+#   else:
+#     print (esraa[n])
+# v1= int(input('please enter a number'))
+# v2= int(input('please enter another number'))
+# v3='Esraa'
+
+# def myfunction(v1,v2):
+#   return v1+v2,v3
+
+# print ('the sum =', myfunction(v1,v2))
+
+#
+# names = ['Alice', 'David', 'Carolina', 9]
+# # >>> lists can take diffrent data types
+# test = ['test']
+# names.append('mohammad')
+# #1 append: add new value to the list at the end
+# # print(names)
+# #2 insert : add new value to the list at a specific chossen index number.
+# names.insert(1, 'khaled')
+# print(names)
+
+#3 extend : add new list to the excisting list at the end of the list
+# list1 = [1, 2, 3, 4, 5,1,1]
+# list2 = [6, 7, 8, 9]
+# list2.extend(list1)
+# list2=list2+list1
+# print(list2)
+
+#4 sum: is used to get the summation of a list
+# can't do summation for a list with diffrent data types inside
+# print(sum(list1))
+
+# print(list1.count(1))
+#5 count: to get the number of times an item is found in a list
+
+# print(len(list1))
+#6 to get the number of items in a list
+
+#7 index: to get the index of the first apperance of a value in a given list
+# print(list1.index(4))
+
+# >>>> print max and min values of list2>>>>
+# maxvalue = 0
+# minvalue = 6
+# for i in list2:
+#   if i > maxvalue:
+#     maxvalue = i
+
+# print(maxvalue)
+# for n in list2:
+#   if n < minvalue:
+#     minvalue = n
+
+# print(minvalue)
+
+#8 max&min : to get the maximum number and minimum number of a list
+# print(max(list2))
+# print(min(list2))
+
+#9 pop: to remove an item based on its index from a given list
+# print(list1.pop(1))
+# print(list1)
+
+# # to remove the last item of a list
+# list1.pop()
+# print(list1)
+
+# # to print the last value of a given list
+# print(list1.pop())
+
+# #10 remove: remove an item form a given list
+# list1.remove(1)
+# print(list1)
+
+# #reverse: used to reverse the items of a list
+# list1.reverse()
+# print(list1)
+
+# list1.sort(reverse=True)
+# print (list1)
+
+#Homework>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# list3 = ['1', 2.5, 3, 'e', 5, 6.001, 7, 0.02, 9, 6]
+# summation = 0
+# for n in  list3 :
+#   try:
+#      summation += float(n)
+#      list3[list3.index(n)]=float(n)
+#   except ValueError:
+#      print(n,'not a number')
+#      list3.remove(n)
+# print(list3)
+# # maxi=max(list3)
+# # mini=min(list3)
+# print('Sum=', summation)
+# print('Avg=', summation / 10)
+# print( 'Maximum number is ',max(list3))
+# print( 'Minimum number is',min(list3))
+
+# print('Index of max. value', list3.index(max(list3)))
+# print('Index of min. value', list3.index(min(list3)))
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
